58b05b2b5b410f201c2638d2d9525382353ed7e8
Valenza <matheusvvalenza@gmail.com>
2020-04-20 13:20:57 -0300

    added fid fibonacci function

--- nodejs-0/src/index.js
+++ nodejs-0/src/index.js
@@ -1,8 +1,21 @@
 'use strict'
 
-const fibonacci = () => null
+const getSum = arr => arr.reduce((acc, crr) => acc + crr);
+
+const fibonacci = (fibonacciParam = [0, 1], maxValue = 350) => {
+    const sum = getSum(fibonacciParam);
+
+    console.log(sum);
+    if (sum > maxValue)
+        return [...fibonacciParam, sum]
+
+    return fibonacci([...fibonacciParam, sum]);
+    
+}
 
 const isFibonnaci = (num) => null
+
+fibonacci()
 
 module.exports = {
     fibonacci,
