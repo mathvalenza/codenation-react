0abd676f8f124847f44b578c59e2854999d75422
Valenza <matheusvvalenza@gmail.com>
2020-04-22 13:57:02 -0300

    refactoring code after code review

--- nodejs-0/src/index.js
+++ nodejs-0/src/index.js
@@ -1,16 +1,23 @@
 'use strict';
 
 const fibonacci = ({ fibonacciParam = [0, 1], maxValue = 350 } = {}) => {
-    const [penultElement, lastElement] = fibonacciParam.slice(-2);
+    const fibonacciLength = fibonacciParam.length;
+
+    const lastElement = fibonacciParam[fibonacciLength-1];
+    const penultElement = fibonacciParam[fibonacciLength-2];
+    
     const newElement = lastElement + penultElement;
+    const newSequence = [...fibonacciParam, newElement];
 
     if (newElement > maxValue)
-        return [...fibonacciParam, newElement];
+        return newSequence;
 
-    return fibonacci({ fibonacciParam: [...fibonacciParam, newElement] });
+    return fibonacci({ fibonacciParam: newSequence });
 }
 
 const isFibonnaci = (num) => fibonacci({ maxValue: num }).includes(num);
+
+console.log(fibonacci(450))
 
 module.exports = {
     fibonacci,
