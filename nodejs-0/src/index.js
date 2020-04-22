5e4d55a0221679accde8a4f45ab803c3582ab339
Valenza <matheusvvalenza@gmail.com>
2020-04-22 13:59:35 -0300

    removing console.log

--- nodejs-0/src/index.js
+++ nodejs-0/src/index.js
@@ -5,7 +5,7 @@ const fibonacci = ({ fibonacciParam = [0, 1], maxValue = 350 } = {}) => {
 
     const lastElement = fibonacciParam[fibonacciLength-1];
     const penultElement = fibonacciParam[fibonacciLength-2];
-    
+
     const newElement = lastElement + penultElement;
     const newSequence = [...fibonacciParam, newElement];
 
@@ -16,8 +16,6 @@ const fibonacci = ({ fibonacciParam = [0, 1], maxValue = 350 } = {}) => {
 }
 
 const isFibonnaci = (num) => fibonacci({ maxValue: num }).includes(num);
-
-console.log(fibonacci(450))
 
 module.exports = {
     fibonacci,
