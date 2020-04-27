101106d500cf871d75b7130e54016eaec3f0b419
Valenza <matheusvvalenza@gmail.com>
2020-04-27 19:53:40 -0300

    initial commmit of nodejs-12

---
+++ nodejs-12/src/index.js
@@ -0,0 +1,7 @@
+const promotions = ['SINGLE LOOK', 'DOUBLE LOOK', 'TRIPLE LOOK', 'FULL LOOK'];
+
+function getShoppingCart(ids, productsList) {
+	return {};
+}
+
+module.exports = { getShoppingCart };
