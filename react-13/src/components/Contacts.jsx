edad721cd7e871ebafaeb64bf070ec5347c6984a
Valenza <matheusvvalenza@gmail.com>
2020-05-07 18:53:59 -0300

    added api fetch

--- react-13/src/components/Contacts.jsx
+++ react-13/src/components/Contacts.jsx
@@ -3,11 +3,25 @@ import React from "react";
 import Contact from './Contact';
 
 class Contacts extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			contacts: [],
+		};
+	}
+	
+	componentDidMount() {
+		fetch('https://5e82ac6c78337f00160ae496.mockapi.io/api/v1/contacts')
+			.then((response) => response.json())
+			.then((contacts) => this.setState({ contacts }));
+	}
+
 	render() {
 		return (
 			<div data-testid="contacts" className="container">
 				<section className="contacts">
-					<Contact />
+					{this.state.contacts.map(contact => <Contact />)}
 				</section>
 			</div>
 		);
