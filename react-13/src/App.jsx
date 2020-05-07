807781f1aa55aa7fe013febc34bc343a6c57dbdc
Valenza <matheusvvalenza@gmail.com>
2020-05-06 21:46:05 -0300

    added integration to fetch contacts

--- react-13/src/App.jsx
+++ react-13/src/App.jsx
@@ -5,6 +5,19 @@ import { ReactComponent as LogoSvg } from "./assets/img/logo.svg";
 import './App.scss';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      contacts: []
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://5e82ac6c78337f00160ae496.mockapi.io/api/v1/contacts')
+      .then((response) => response.json())
+      .then(contacts => this.setState({ contacts }));
+  }
+
   render() {
     return (
       <React.Fragment>
