d4f8dabe1015e9b1ae9f13f3870699dc06bf0d3e
Valenza <matheusvvalenza@gmail.com>
2020-05-07 21:49:31 -0300

    formating date

--- react-13/src/components/Contact.jsx
+++ react-13/src/components/Contact.jsx
@@ -8,7 +8,7 @@ class Contact extends React.Component {
         <span className="contact__data">{this.props.name}</span>
         <span className="contact__data">{this.props.phone}</span>
         <span className="contact__data">{this.props.country}</span>
-        <span className="contact__data">{this.props.admissionDate}</span>
+        <span className="contact__data">{new Date(this.props.admissionDate).toLocaleDateString()}</span>
         <span className="contact__data">{this.props.company}</span>
         <span className="contact__data">{this.props.department}</span>
       </article>
