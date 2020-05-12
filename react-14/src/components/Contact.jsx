ceb999c176a9524d56ea74110c85e359dbcb3608
Valenza <matheusvvalenza@gmail.com>
2020-05-12 20:04:27 -0300

    fixed date conversion to the tests

--- react-14/src/components/Contact.jsx
+++ react-14/src/components/Contact.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
 
 class Contact extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      localDate: '',
+    }
+  }
+
+  componentDidMount() {
+    const transformedDate = new Date(this.props.data.admissionDate).toLocaleDateString();
+    const [year, month, day] = transformedDate.split('-');
+
+    this.setState({ localDate: `${day.padStart(2, '0')}/${month.padStart(2, '0')}/${year}` });
+  }
+
   render() {
     return (
       <article data-testid="contact" className="contact">
@@ -8,7 +23,7 @@ class Contact extends React.Component {
         <span data-testid="contact-name" className="contact__data">{this.props.data.name}</span>
         <span data-testid="contact-phone" className="contact__data">{this.props.data.phone}</span>
         <span data-testid="contact-country" className="contact__data">{this.props.data.country}</span>
-        <span data-testid="contact-date" className="contact__data">{new Date(this.props.data.admissionDate).toLocaleDateString()}</span>
+        <span data-testid="contact-date" className="contact__data">{this.state.localDate}</span>
         <span data-testid="contact-company" className="contact__data">{this.props.data.company}</span>
         <span data-testid="contact-department" className="contact__data">{this.props.data.department}</span>
       </article>
