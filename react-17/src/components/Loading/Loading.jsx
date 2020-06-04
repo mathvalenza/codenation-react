d962497840c5bceec54ac6731d7a6e27cdda85a0
Valenza <matheusvvalenza@gmail.com>
2020-06-04 20:05:34 -0300

    renamed mock and added data-testid

--- react-17/src/components/Loading/Loading.jsx
+++ react-17/src/components/Loading/Loading.jsx
@@ -5,7 +5,7 @@ import './Loading.scss';
 
 const Loading = ({ text }) => (
   <React.Fragment>
-    <div className="loading">
+    <div className="loading" data-testid="loading">
       <span />
       <span />
       <span />
@@ -16,19 +16,16 @@ const Loading = ({ text }) => (
       <span />
     </div>
 
-    <p className="loading__text">
-      {text}
-    </p>
+    <p className="loading__text">{text}</p>
   </React.Fragment>
 );
 
 Loading.propTypes = {
-  text: PropTypes.string,
-}
+  text: PropTypes.string
+};
 
 Loading.defaultProps = {
-  text: 'Carregando...',
-}
-
+  text: 'Carregando...'
+};
 
 export default Loading;
